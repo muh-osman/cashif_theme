44c1a8f7ad87b635b39633003264e922988b82a5
muh-osman <mr.muhammed.osman@gmail.com>
2024-07-17 15:29:10 +0300

    Fix

--- functions.php
+++ functions.php
@@ -5,36 +5,31 @@
  */
 function wpdocs_cashif_theme_scripts()
 {
-    // CSS
-    wp_enqueue_style('bootstrap_css', get_template_directory_uri() . '/assets/css/bootstrap.min.css');
-    wp_enqueue_style('aos_css', get_template_directory_uri() . '/assets/css/aos.css');
-    wp_enqueue_style('plyr_css', 'https://cdn.plyr.io/3.6.8/plyr.css');
-    wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/style.css');
-    // Font
-    wp_enqueue_style('font', 'https://fonts.googleapis.com');
-    wp_enqueue_style('font_two', 'https://fonts.gstatic.com');
-    wp_enqueue_style('font_three', 'https://fonts.googleapis.com/css2?family=Alexandria:wght@300;400;500;600;700&display=swap');
+    // Enqueue CSS
+    wp_enqueue_style('bootstrap-css', get_template_directory_uri() . '/assets/css/bootstrap.min.css');
+    wp_enqueue_style('aos-css', get_template_directory_uri() . '/assets/css/aos.min.css');
+    wp_enqueue_style('plyr-css', get_template_directory_uri() . '/assets/css/plyr.min.css');
+    wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/style.min.css');
 
+    // Enqueue Google Fonts
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Alexandria:wght@300;400;500;600;700&display=swap', array(), '1.0.0');
 
-
-    // JS
+    // Enqueue JS
     wp_enqueue_script('jquery');
-    wp_enqueue_script('bootstrap_js', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js', array('jquery'), '1.0.0', true);
-    wp_enqueue_script('aos_js', get_template_directory_uri() . '/assets/js/aos.js', array('jquery'), '1.0.0', true);
-    wp_enqueue_script('plyr_js', 'https://cdn.plyr.io/3.6.8/plyr.js', array('jquery'), '3.6.8', true);
-    wp_enqueue_script('app', get_template_directory_uri() . '/assets/js/app.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('aos-js', get_template_directory_uri() . '/assets/js/aos.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('plyr-js', get_template_directory_uri() . '/assets/js/plyr.min.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('app-js', get_template_directory_uri() . '/assets/js/app.min.js', array('jquery'), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'wpdocs_cashif_theme_scripts');
 
 
 
 
-
-
-// Register a new navigation menu
-function add_Main_Nav()
-{
-    register_nav_menu('header-menu', __('Header Menu'));
-}
-// Hook to the init action hook, run our navigation menu function
-add_action('init', 'add_Main_Nav');
+// // Register a new navigation menu
+// function add_Main_Nav()
+// {
+//     register_nav_menu('header-menu', __('Header Menu'));
+// }
+// // Hook to the init action hook, run our navigation menu function
+// add_action('init', 'add_Main_Nav');
--- header.php
+++ header.php
@@ -9,10 +9,10 @@
     <!-- Title -->
     <title><?php bloginfo('name') ?> | <?php bloginfo('description') ?></title>
 
+
+    <!-- yoast seo will overwrite all this things -->
     <!-- Meta Description -->
     <meta name="description" content="كاشف لفحص السيارات | مركز متخصص في فحص السيارات المستعملة، يقدم مفهوم جديداً يواكب أحدث التقنيات ليساعدك في قرار الشراء">
-
-
 
     <!-- Favicon -->
     <?php
@@ -22,6 +22,7 @@
     }
     ?>
 
+    <meta name="facebook-domain-verification" content="afn8s5scmy0pcaoswyiz34ncdqymfr" />
 
     <?php wp_head() ?>
 </head>
