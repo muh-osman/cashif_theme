82e35da08d9eb9326dcbc11efce7642085467098
muh-osman <mr.muhammed.osman@gmail.com>
2026-02-22 01:41:49 +0300

    Fix

--- assets/js/tessst.js
+++
@@ -1,5 +0,0 @@
-// http://localhost/cashif/pay/?plan=شامل&year_id=2&car_model_id=335&price_id=0&full_year=2018
-// http://localhost/cashif/pay/en/?plan=Engine&year_id=2&car_model_id=335&price_id=2&full_year=2018
-
-// http://localhost/cashif/check-it/receipt/?plan=شامل&year_id=2&car_model_id=324&price_id=0&full_year=2018
-// http://localhost/cashif/check-it/receipt/en/?plan=Basic&year_id=2&car_model_id=324&price_id=0&full_year=2018
